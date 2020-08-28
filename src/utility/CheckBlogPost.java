0f4244423c506986b06e6307c5444a7bf4905089
Chaitra6 <48185350+Chaitra6@users.noreply.github.com>
2020-08-28 22:37:29 +0530

    FSJ0820A003

--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,47 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface {
+
+	String[] badWords = { "asshole", "alabama hot pocket", "alaskan pipeline", "anal", "anal impaler", "anal leakage",
+			"balls", "bangbros", "bareback", "barely legal", "barenaked", "barf", "bastard", "blowjob", "c.0.c.k",
+			"c.o.c.k", "c.u.n.t", "c0ck", "c-0-c-k", "c0cksucker", "caca", "cacafuego", "cahone", "camel toe",
+			"cameltoe", "camgirl", "camslut" };
+
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		String title = blog.getBlogTitle();
+		for(int i=0;i<badWords.length;i++) {
+			if(title == badWords[i]) {
+				return false;
+			}
+		}
+		return true;		
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		String description = blog.getBlogDescription();
+		for(int i=0;i<badWords.length;i++) {
+			if(description == badWords[i]) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+	@Override
+	public boolean checkBlog(Blog blog) {
+		boolean blogTitle = checkBlogTitle(blog);
+		boolean blogDesc = checkBlogDescription(blog);
+
+		if (blogTitle && blogDesc) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
